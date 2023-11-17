31e7543fbf176996f53048ef11b6e53fa33c1fc4
younji66 <youn37@kyonggi.ac.kr>
2023-11-17 14:03:58 +0900

    DB

---
+++ src/main/java/com/dto/memberDTO.java
@@ -0,0 +1,13 @@
+package com.dto;
+
+import lombok.Data;
+
+@Data
+public class memberDTO {
+    private long userid;
+    private String name;
+    private String email;
+    private String kakao_id;
+    private String phone;
+
+}
